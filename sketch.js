a80afcd4ed6b796ae8bd151f7a7c6a157c480e83
Guilherme Ribeiro da Costa <Ribeiro.guicosta@gmail.com>
2023-02-02 12:18:16 -0300

    Consolidando conceitos e treinando lógica com JS

---
+++ sketch.js
@@ -0,0 +1,47 @@
+//Ball Atributes
+let ballX = 400;
+let ballY = 300;
+let ballDiameter = 25;
+let ballRadius = ballDiameter / 2;
+//Ball Move Atributes
+let speedX = 5;
+let speedY = 5;
+let moveX = ballX + speedX;
+//Player Atributes
+let playerRacketX = 10;
+let playerRacketY = 300;
+let playerRacketWidth = 10;
+let playerRacketHeight = 90;
+
+function act() {
+  drawBall();
+  drawPlayerRacket();
+  movimentation();
+  bounceAtEdge();
+}
+function bounceAtEdge() {
+  if (ballX - ballRadius < 0 || ballX + ballRadius > width) {
+    speedX *= -1;
+  }
+  if (ballY - ballRadius < 0 || ballY + ballRadius > height) {
+    speedY *= -1;
+  }
+}
+function movimentation() {
+  ballX += speedX;
+  ballY += speedY;
+}
+function drawBall() {
+  circle(ballX, ballY, ballDiameter);
+}
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(0);
+  act();
+}
+function drawPlayerRacket(){
+  rect(playerRacketX, playerRacketY, playerRacketWidth, playerRacketHeight);
+}
